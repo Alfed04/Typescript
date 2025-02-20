3954411cc8aa2c59a526ba66010658a2a087dc4e
Alfed Khan <98413980+Alfed04@users.noreply.github.com>
2025-02-20 13:42:11 +0500

    Update assignment.ts

--- src/assignment.ts
+++ src/assignment.ts
@@ -1,29 +1,29 @@
 // Create two types called User and Admin
 // Create a function that takes either a user  or an admin as an input , and returns a string saying "Welcome ,  [name]"
 
-// type User = {
-//     name: string;
-//     services:string;
-// }
-// type Admin = {
-//     name: string;
-//     priviledges:string;
-// }
-// type UserOrAdmin = User | Admin
+type User = {
+    name: string;
+    services:string;
+}
+type Admin = {
+    name: string;
+    priviledges:string;
+}
+type UserOrAdmin = User | Admin
 
-// let user:User ={
-//     name : "Alfed",
-//     services:"Netflix Pro User"
-// }
-// let admin:Admin ={
-//     name : "Alfed",
-//     priviledges:"Make Content or Delete Content"
-// }
+let user:User ={
+    name : "Alfed",
+    services:"Netflix Pro User"
+}
+let admin:Admin ={
+    name : "Alfed",
+    priviledges:"Make Content or Delete Content"
+}
 
-// function Fn(user:UserOrAdmin){
-//     return  "Welcome , "+user.name;
-// }
+function Fn(user:UserOrAdmin){
+    return  "Welcome , "+user.name;
+}
 
-// console.log(Fn(user))
-// console.log(Fn(admin))
+console.log(Fn(user))
+console.log(Fn(admin))
 
